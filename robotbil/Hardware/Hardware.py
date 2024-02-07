2bdc49b28adbecf9e4006ef498b34cfa53302dc9
UwU ( °ω°  :3 <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-07 11:59:09 +0100

    ide til drejning

--- robotbil/Hardware/Hardware.py
+++ robotbil/Hardware/Hardware.py
@@ -31,3 +31,20 @@ def tilbage():
     højreTilbage.value(1)
 
 
+def drejhøjre(grader):
+    venstreFrem.value(1)
+    højreFrem.value(0)
+    venstreTilbage.value(0)
+    højreTilbage.value(1)
+    time.sleep(grader)
+    venstreFrem.value(0)
+    højreTilbage.value(0)
+
+def drejvenstre(grader):
+    venstreFrem.value(0)
+    højreFrem.value(1)
+    venstreTilbage.value(1)
+    højreTilbage.value(0)
+    time.sleep(grader)
+    højreFrem.value(0)
+    venstreTilbage.value(0)

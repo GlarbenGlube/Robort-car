8e769c31e0e9361d5a0ebd0e9727b430d62fcad1
Lucas <jensenlucas19@gmail.com>
2024-02-08 11:41:16 +0100

    WIP GUI interface ny Quit knap

--- PC-SW/GUI.py
+++ PC-SW/GUI.py
@@ -13,17 +13,19 @@ def GUI():
                sg.Button('Stop', size=(12, 2), pad=(10, 10), font='Impact'),
                sg.Button('Højre', size=(12, 2), pad=(10, 10), font='Impact')],
 
-              [sg.Button('Tilbage', size=(12, 2), pad=(10, 10), font='Impact')]]
+              [sg.Button('Tilbage', size=(12, 2), pad=(10, 10), font='Impact')],
+
+              [sg.Button('Quit', size=(12, 2), pad=(10, 50), font='Impact')]]
 
     centering = [[sg.VPush()],
                  [sg.Push(), sg.Column(layout, element_justification='c'), sg.Push()],
                  [sg.VPush()]]
 
-    window = sg.Window('Window Title', centering, size=(700, 500))
+    window = sg.Window('Window Title', centering, size=(900, 600))
 
     while True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED:
+        if event == sg.WIN_CLOSED or 'quit':
             break
         else:
             print(event)

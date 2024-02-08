203b4d836ad5a2f14ad1e99d1fcbf0e2b5fc6ba9
Lucas <jensenlucas19@gmail.com>
2024-02-08 12:14:51 +0100

    quaso

--- PC-SW/Remote.py
+++ PC-SW/Remote.py
@@ -4,11 +4,10 @@ def remote(command: str):
     com = { "forward":1  ,}
     if command in com:
         UDPSend(com[command])
-        
 def UDPSend(cmd):
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    while True:
 
-        sock.sendto(bytes(cmd, "utf-8"), ('10.120.0.8' , 5001))
+    sock.sendto(bytes(cmd, "utf-8"), ('10.120.0.8' , 5001))
 
-UDPSend("forward")
+while True:
+    UDPSend(input())

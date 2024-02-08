fac8cf06f278978ea2732ef42fe2437a10a32dd6
Lucas <jensenlucas19@gmail.com>
2024-02-08 09:26:45 +0100

    WIP GUI interface

---
+++ robotbil/UserInterface/GUI.py
@@ -0,0 +1,45 @@
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')
+
+def GUI():
+    column_to_be_centered = [[sg.Button('Follow Wall', size=(12, 2), pad=(10, 50), font='Impact'),
+                             sg.Button('Push Object', size=(12, 2), pad=(10, 50), font='Impact'),
+                             sg.Button('Manuel', size=(12, 2), pad=(10, 50), font='Impact')],
+
+                             [sg.Button('Frem', size=(12, 2), pad=(10,10), font='Impact')],
+
+                              [sg.Button('Venstre', size=(12, 2), pad=(10,10), font='Impact'),
+                               sg.Button('Stop', size=(12, 2), pad=(10,10), font='Impact'),
+                               sg.Button('Højre', size=(12, 2), pad=(10,10), font='Impact')],
+
+                              [sg.Button('Tilbage', size=(12, 2), pad=(10,10), font='Impact')]]
+
+    layout = [[sg.VPush()],
+              [sg.Push(), sg.Column(column_to_be_centered,element_justification='c'), sg.Push()],
+              [sg.VPush()]]
+
+    window = sg.Window('Window Title', layout, size=(700,500))
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == 'Frem':
+            print("frem")
+        elif event == 'Højre':
+            print("Højre")
+        elif event == 'Stop':
+            print("stop")
+        elif event == 'Venstre':
+            print("venstre")
+        elif event == 'Tilbage':
+            print("tilbage")
+        elif event == 'Push Object':
+            print("push")
+        elif event == 'Follow Wall':
+            print("follow wall")
+        elif event == 'Manuel':
+            print("manuel")
+
+    window.close()

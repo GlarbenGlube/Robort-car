2583d4d0448abda7f0daf9c20725ee9b4f66d761
ApieceofPaper-beginner <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-11 23:00:50 +0100

    kommer reset og maual override funtionen ikke til at ligge i gui?

---
+++ robotbil/UserInterface/kommandoer/SUMO.py
@@ -0,0 +1,13 @@
+#skal kunne følge en varierende væg og dreje så bilen kan følge væggen
+#skeletkode
+from Hardware import ReadSensor as SA
+from Hardware import motorstyrring as ms
+from time import sleep
+def SUMO():
+    while True:
+        while SA.measureDistance > 100:
+            ms.onspotturnleft()
+        if refleks == gulv:
+            ms.forward()
+        ms.stop()
+        ms.onspotturnleft()
--- robotbil/UserInterface/kommandoer/followWall.py
+++ robotbil/UserInterface/kommandoer/followWall.py
@@ -1,4 +1,26 @@
-#skal kunne følge en carierende væg og dreje så bilen kan følge væggen
+#skal kunne følge en varierende væg og dreje så bilen kan følge væggen
+#skeletkode
 from Hardware import ReadSensor as SA
+from Hardware import motorstyrring as ms
+from time import sleep
 def follow():
+    side = input("what side is the wall on? left or right: ")
+    if side == left:
+        while True:
+            if #readin == stopsignal:
+                break
+            elif SA.measureDistance() > 100:
+                ms.turnleft(SA.measureDistance()*0.5)
+                ms.forward()
+                sleep(1)
+                ms.stop()
+    else:
+        while True:
+            if #readin == stopsignal:
+                break
+            elif SA.measureDistance() > 100:
+                ms.turnright(SA.measureDistance()*0.5)
+                ms.forward()
+                sleep(1)
+                ms.stop()
     pass

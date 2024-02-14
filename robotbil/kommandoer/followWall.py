096d61a5e153f4ef848a73855071d71d5a004b8c
UwU ( °ω°  :3 <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-14 11:20:42 +0100

    wallfollow update

--- robotbil/kommandoer/followWall.py
+++ robotbil/kommandoer/followWall.py
@@ -1,12 +1,12 @@
 #skal kunne følge en varierende væg og dreje så bilen kan følge væggen
 #skeletkode
-from Hardware import ReadSensor as RS
-from Hardware import motorstyrring as ms
-from Hardware import Poweroffbutton as pob
+import robotbil.Hardware.ReadSensor as RS
+import robotbil.Hardware.motorstyrring as ms
+import robotbil.Hardware.Poweroffbutton as pob
 from time import sleep
 def follow():
-    side = input("what side is the wall on? left or right: ")
-    if side == "left":
+    #side = input("what side is the wall on? left or right: ")
+    """if side == "left":
         while True:
             if pob.readbutton == "ON":
                 break
@@ -15,13 +15,13 @@ def follow():
                 ms.forward()
                 sleep(1)
                 ms.stop()
-    else:
-        while True:
-            if pob.readbutton == "ON":
-                break
-            elif RS.measureDistance() > 100:
-                ms.VariableRight(RS.measureDistance()*0.5)
-                ms.forward()
-                sleep(1)
-                ms.stop()
+    else:"""
+    while True:
+        if pob.readbutton == "ON":
+            break
+        elif RS.measureDistance() > 100:
+            ms.VariableRight(RS.measureDistance()*0.5)
+            ms.forward()
+            sleep(1)
+            ms.stop()
     pass

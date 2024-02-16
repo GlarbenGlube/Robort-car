63a2a6d71cfe18a078f33b91429bef900ed1a011
UwU ( °ω°  :3 <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-16 12:05:17 +0100

    robotten gav op på at søge

--- robotbil/kommandoer/SUMO.py
+++ robotbil/kommandoer/SUMO.py
@@ -7,14 +7,21 @@ from time import sleep
 reflectionThreshold = 40000
 
 def SUMO():
+    i = 0
+
     while B.button() != "ON":
         
         dutycycleR = 0.5
         M.UpdatePWM(None, dutycycleR)
-        
+        o = 0
         while S.measureDistance > 100 and B.button() != "ON":
-            M.turnleft()
+            i += 1
+            M.turnright()
             sleep(0.5)
+            if i == 20:
+                while S.measureQA() <= reflectionThreshold or o > 5:
+                    o += 1
+                    forward()
 
         M.UpdatePWM(None, 1)
         

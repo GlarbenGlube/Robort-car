842e6b83f03fe76af21aee1f92bb77f113724f38
UwU ( °ω°  :3 <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-19 10:18:05 +0100

    sketch til start af followwall

--- robotbil/kommandoer/followWall.py
+++ robotbil/kommandoer/followWall.py
@@ -18,7 +18,7 @@ def follow():
                 ms.forward()
                 sleep(1)
                 ms.stop()
-    else:"""
+    else:
     while pob.readbutton != "ON":
         while RS.measureDistance() > 100 and pob.readbutton != "ON":
                 ms.VariableRight(RS.measureDistance()*0.5)
@@ -26,3 +26,23 @@ def follow():
                 sleep(1)
                 ms.stop()
     pass
+"""
+def followwall():
+    while pob.readbutton != "ON":
+        ms.UpdatePWM(1200,dutyL=0.4,dutyR=0.4)
+        print("start")
+        if RS.measureDistance() > 100:
+            dis = RS.measureDistance()
+            if dis > 100 and dis < 50:
+                ms.VariableLeft(RS.measureDistance() * 0.5)
+                ms.forward()
+                sleep(1)
+                ms.stop()
+            else:
+                ms.VariableLeft(RS.measureDistance() * 0.5)
+                sleep(1)
+                ms.stop()
+# tænder sensoren
+# læser afstanden
+# gør noget afhængig af afstand. hvis den er mindre end grænseværdien drej til venstre og ret op hvis den er større end grænseværdien drej til venstre
+# hvis afstanden er endnu højere eller lavere end de 2 grænseværdier. drej hårdere.

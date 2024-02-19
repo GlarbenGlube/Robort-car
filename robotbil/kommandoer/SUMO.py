079febed5ad92f44347d0546d168a5f9eea48848
UwU ( °ω°  :3 <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-19 09:39:41 +0100

    SUMO opdateret med edging

--- robotbil/kommandoer/SUMO.py
+++ robotbil/kommandoer/SUMO.py
@@ -5,7 +5,7 @@ from Hardware import motorstyrring as M
 from Hardware import Poweroffbutton as B
 from time import sleep
 reflectionThreshold = 40000
-
+""""
 def SUMO():
     i = 0
 
@@ -38,3 +38,40 @@ def SUMO():
         sleep(0.6)
         M.turnright()
         sleep(0.2)
+"""""
+def cbt():
+    while True:
+        M.UpdatePWM(1200,dutyL=0.4,dutyR=0.4)
+        print("start")
+        b = 0
+        distance = S.measureDistance()
+        while b < 25:
+            distance = S.measureDistance()
+            if distance < 110:
+                b=31
+                if distance < 10:
+                    M.turnright()
+                    sleep(0.25 - distance/100)
+                    M.stop()
+                else:
+                    M.turnleft()
+                    sleep(distance/100)
+                    M.stop()
+            M.turnright()
+            b+=1
+        M.UpdatePWM(1000,dutyL=0.615,dutyR=0.6)
+        reflection = S.measureReflection()
+        while reflection <= 40000:
+            print(reflection)
+            M.forward()
+            reflection = S.measureReflection()
+        M.back()
+        sleep(0.05)
+        print("return")
+        M.UpdatePWM(1000,dutyL=0.52,dutyR=0.5)
+        M.back()
+        sleep(1)
+        M.stop()
+        M.turnright()
+        sleep(0.5)
+        M.stop()

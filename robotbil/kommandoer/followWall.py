337bc789b3a34c497d2b3dd95720f0da3a33b883
UwU ( °ω°  :3 <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-19 10:53:40 +0100

    wallfollow comments

--- robotbil/kommandoer/followWall.py
+++ robotbil/kommandoer/followWall.py
@@ -27,30 +27,51 @@ def follow():
                 ms.stop()
     pass
 """
+
+
 def followwall():
+    # Checks if button is pressed
     while pob.readbutton != "ON":
-        ms.UpdatePWM(1200,dutyL=0.4,dutyR=0.4)
+        # Sets the speed of the motors
+        ms.UpdatePWM(1200, dutyL=0.4, dutyR=0.4)
+
+        # Prints "start" message
         print("start")
+
+        # Measures the distance from the wall
         dis = RS.measureDistance()
+
+        # If the measured distance is greater than 100
         if dis > 100:
+            # Measures the distance again
             dis = RS.measureDistance()
+
+            # If the distance is greater than 100 and less than 50
             if dis > 100 and dis < 50:
+                # Adjusts the left motor speed based on distance and moves forward
                 ms.VariableLeft(RS.measureDistance() * 0.5)
                 ms.forward()
                 sleep(1)
                 ms.stop()
             else:
+                # Adjusts the left motor speed based on distance and stops
                 ms.VariableLeft(RS.measureDistance() * 0.5)
                 sleep(1)
                 ms.stop()
+        # If the measured distance is less than 100
         else:
+            # Measures the distance again
             dis = RS.measureDistance()
+
+            # If the distance is less than 100 and greater than 150
             if dis < 100 and dis > 150:
+                # Adjusts the left motor speed based on distance and moves forward
                 ms.VariableLeft(RS.measureDistance() * 0.5)
                 ms.forward()
                 sleep(1)
                 ms.stop()
             else:
+                # Adjusts the left motor speed based on distance and stops
                 ms.VariableLeft(RS.measureDistance() * 0.5)
                 sleep(1)
                 ms.stop()

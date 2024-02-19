6b95d70da6584bb42244f670d69602bfab43694d
UwU ( °ω°  :3 <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-19 10:20:28 +0100

    sketch til start af followwall

--- robotbil/kommandoer/followWall.py
+++ robotbil/kommandoer/followWall.py
@@ -31,9 +31,21 @@ def followwall():
     while pob.readbutton != "ON":
         ms.UpdatePWM(1200,dutyL=0.4,dutyR=0.4)
         print("start")
-        if RS.measureDistance() > 100:
+        dis = RS.measureDistance()
+        if dis > 100:
             dis = RS.measureDistance()
             if dis > 100 and dis < 50:
+                ms.VariableLeft(RS.measureDistance() * 0.5)
+                ms.forward()
+                sleep(1)
+                ms.stop()
+            else:
+                ms.VariableLeft(RS.measureDistance() * 0.5)
+                sleep(1)
+                ms.stop()
+        else:
+            dis = RS.measureDistance()
+            if dis < 100 and dis > 150:
                 ms.VariableLeft(RS.measureDistance() * 0.5)
                 ms.forward()
                 sleep(1)

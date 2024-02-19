98e0f77515c23366f132171070d0cc7338d797ef
Wemew <37046647+Wemew@users.noreply.github.com>
2024-02-19 11:02:11 +0100

    Comments for robotbil UDP

--- robotbil/Connection/UDP.py
+++ robotbil/Connection/UDP.py
@@ -6,25 +6,27 @@ def UDPConnect():
 # Initialize network
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.connect('ITEK 2nd', '2nd_Semester_F24v')
-    wlandelay = time.ticks_ms() + 10000
+    wlan.connect('ITEK 2nd', '2nd_Semester_F24v')   
+    wlandelay = time.ticks_ms() + 10000     # Set timeout for connection
+    
     led = Pin("LED", Pin.OUT)
 
-    # Check Wi-Fi connection
+    # Wait for Wi-Fi connection, continue when connected
     while time.ticks_ms() < wlandelay:
         if wlan.isconnected():
             if wlan.status() < 0 or wlan.status() >= 3:
                 break
         idle()
 
-    if wlan.status() != 3:
-        led.toggle()
+
+    if wlan.status() != 3:                  # If wlan doesn't connect
+        led.off()                           # Change led state
         print('Wi-Fi connection failed')
         reset()
     else:
-        led.toggle()
-        print('Connected')
+        led.on()                            # Indicate connection success
+        print('Connected')                  # Indicate connection success
         status = wlan.ifconfig()
-        print('ip = ' + status[0])
-        wlan_mac = wlan.config('mac')
-        print("MAC Address:", wlan_mac)  # Show MAC for peering
+        print('ip = ' + status[0])          # Show ip for connecting with GUI
+        wlan_mac = wlan.config('mac')       
+        print("MAC Address:", wlan_mac)     # Show MAC for peering

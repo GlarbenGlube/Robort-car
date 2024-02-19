a18cc5737e7abfa6c99bc7a868e7a933fdbf5b51
Lucas <jensenlucas19@gmail.com>
2024-02-19 10:59:03 +0100

    kommentar tilføjet til SUMO

--- robotbil/kommandoer/SUMO.py
+++ robotbil/kommandoer/SUMO.py
@@ -40,38 +40,49 @@ def SUMO():
         sleep(0.2)
 """""
 def SUMO():
+    # set initial motor speed and direction
     while True:
         M.UpdatePWM(1200,dutyL=0.4,dutyR=0.4)
         print("start")
         b = 0
+        # measures distance to obstacle
         distance = S.measureDistance()
         while b < 25:
+            # Continuously measure distance to adjust movement
             distance = S.measureDistance()
             if distance < 110:
                 b=31
                 if distance < 10:
+                    # If obstacle is very close, turn right with a delay proportional to distance
                     M.turnright()
                     sleep(0.25 - distance/100)
                     M.stop()
                 else:
+                    # If obstacle is near, turn left with a delay proportional to distance
                     M.turnleft()
                     sleep(distance/100)
                     M.stop()
+            # Increment counter for number of turns
             M.turnright()
             b+=1
+        # Adjust motor speed for obstacle avoidance
         M.UpdatePWM(1000,dutyL=0.615,dutyR=0.6)
         reflection = S.measureReflection()
+        # Move forward until an object is detected
         while reflection <= 40000:
             print(reflection)
             M.forward()
             reflection = S.measureReflection()
+        # Back off slightly
         M.back()
         sleep(0.05)
         print("return")
+        # Adjust motor speed for returning back
         M.UpdatePWM(1000,dutyL=0.52,dutyR=0.5)
         M.back()
         sleep(1)
         M.stop()
+        # Turn right before restarting the loop
         M.turnright()
         sleep(0.5)
         M.stop()

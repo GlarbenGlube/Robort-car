900fd3fcb55e1c4f69ff9d3b596faae1d98eaa97
UwU ( °ω°  :3 <151613664+ApieceofPaper-beginner@users.noreply.github.com>
2024-02-21 12:08:47 +0100

    UART exampel.2

--- robotbil/edging.py
+++ robotbil/edging.py
@@ -51,3 +51,18 @@ uart.init(bits=8, parity=None, stop=2)
 uart.write(int('0x5A'))
 uart.write(int('0x51'))
 uart.write(int('0xF6'))
+''''''''''
+from machine import Pin,UART
+import time
+uart = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+uart.init(bits=8, parity=None, stop=2)
+led = Pin("LED", Pin.OUT)
+
+while True:
+    uart.write('t')
+    if uart.any(): 
+        data = uart.read() 
+        if data== b'm':
+            led.toggle() 
+    time.sleep(1)
+'''''
